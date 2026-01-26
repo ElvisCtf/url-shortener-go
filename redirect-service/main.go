bf91a03b7d51bdfe22ea925aed1c126b8b6a3574
ElvisCtf <elvischengtf@gmail.com>
2026-01-26 18:02:23 +0800

    refactor: Read environment variables by env_file in docker-compose and remove godotenv

--- redirect-service/main.go
+++ redirect-service/main.go
@@ -7,14 +7,9 @@ import (
 	"redirect-service/internal/router"
 	"redirect-service/internal/service"
 	"redirect-service/internal/util"
-
-	"github.com/joho/godotenv"
 )
 
 func main() {
-	if err := godotenv.Load(".env"); err != nil {
-        log.Println("No .env file found")
-    }
 	addr := util.Env("ADDR", ":8081")
 
 	repo := repository.NewRepo()
--- shorten-service/main.go
+++ shorten-service/main.go
@@ -7,17 +7,12 @@ import (
 	"shorten-service/internal/router"
 	"shorten-service/internal/service"
 	"shorten-service/internal/util"
-
-	"github.com/joho/godotenv"
 )
 
 func main() {
-	if err := godotenv.Load(".env"); err != nil {
-        log.Println("No .env file found")
-    }
     addr := util.Env("ADDR", ":8080")
     baseURL := util.Env("BASE_URL", "http://localhost:8081")
-    storage := util.Env("STORAGE", "postgre")
+    storage := util.Env("STORAGE", "")
 
     repo := repository.NewRepo(storage)
     service := service.NewShorten(baseURL, repo)
